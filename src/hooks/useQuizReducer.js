8ee14cb3092eaa648916788eb327ce15eaae1b09
phaman <phaman@localhost.localdomain>
2025-08-03 20:40:45 +0700

    feat: update NEXT_QUESTION reducer to save full submitted answer with correctness info

--- src/hooks/useQuizReducer.js
+++ src/hooks/useQuizReducer.js
@@ -15,10 +15,20 @@ export function quizReducer(state, action) {
         }
         case NEXT_QUESTION: {
             const isLastQuestion = state.currentQuestionIndex === QUESTIONS.length - 1;
+            // câu hỏi hiện tại
+            const currentQuestion = QUESTIONS[state.currentQuestionIndex];
+
+            const submittedAnswer = {
+                questionId: currentQuestion.id,
+                selectedAnswer: state.currentSelectedAnswer,
+                correctAnswer: currentQuestion.correct,
+                isCorrect: state.currentSelectedAnswer === currentQuestion.correct
+            };
+
             return {
                 ...state,
                 currentQuestionIndex: isLastQuestion ? state.currentQuestionIndex : state.currentQuestionIndex + 1,
-                submittedAnswers: [...state.submittedAnswers, state.currentSelectedAnswer],
+                submittedAnswers: [...state.submittedAnswers, submittedAnswer],
                 currentSelectedAnswer: isLastQuestion ? state.currentSelectedAnswer: null,
                 isQuizFinished: isLastQuestion
             };

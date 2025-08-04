0ff8eeefb3c627d3c69ea66d76c683747409d743
phaman <phaman@localhost.localdomain>
2025-08-04 17:31:10 +0700

    refactor: load questions from JSON file instead of hardcoding

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
-import { useState } from "react";
-import {QUESTIONS} from './data'
+import { useState, useEffect } from "react";
+import questionsData from './data/questions.json';
 import Header from "./components/Header";
 import StartButton from "./components/StartButton";
 import Quiz from "./components/Quiz";
 
 function App() {
     const [isStart, setIsStart] = useState(false);
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(() => {
+        const loadQuestions = async () => {
+            await new Promise((r) => setTimeout(r, 500)); // mô phỏng delay
+            setQuestions(questionsData);
+        };
+
+        loadQuestions();  
+    }, []);
+
+
+    console.log(questions);
+
+    if (questions.length === 0) {
+        return <div className="text-white">Loading questions...</div>;
+    }
+    
     return (
         <div className="p-4">
-        <Header />
-        {/* Các component khác */}
-        {!isStart && (
-            <StartButton onStart={() => setIsStart(true)} />
-        )}
-        {isStart && <Quiz questions={QUESTIONS}/>}
-        
-    </div>
-  );
+            <Header />
+            {!isStart && <StartButton onStart={() => setIsStart(true)} />}
+            {isStart && <Quiz questions={questions} />} 
+        </div>
+    );
 }
 
 export default App;

51a7af970da0c3cc9dabd42f51a7e3b7e1d76a40
phaman <phaman@localhost.localdomain>
2025-08-04 17:00:17 +0700

    🐛 Fix setState during render error in QuizHeader by decoupling onTimeout

--- src/components/QuizHeader.jsx
+++ src/components/QuizHeader.jsx
@@ -1,33 +1,43 @@
 import { useRef, useState, useEffect } from "react";
 
+const TIME = 30;
+
 export default function QuizHeader({currentQuestionIndex, totalQuestions, onTimeout}){
 
     const timerRef = useRef();
-    const [timeLeft, setTimeLeft ] = useState(1);
+    const intervalRef = useRef();
+
+    const [timeLeft, setTimeLeft ] = useState(TIME);
 
     useEffect(() => {
-    // Code cần chạy khi component mount hoặc update
-    // Bạn lắp đồng hồ
-    timerRef.current = setInterval(
-        () => {
-            //Đồng hồ tự chạy và trừ timeLeft mỗi giây
-            setTimeLeft((prev) => {
-                if(prev <= 0) {
-                    clearInterval(timerRef.current); // dừng khi còn 0
-                    onTimeout?.(); // gọi callback khi hết giờ
-                    return 0;
-                }
-                return prev -1;
-            })
-        }, 1000
-    );
-    return () => {
-            // Code cleanup (chạy khi component unmount hoặc trước khi chạy lại useEffect)
-            // Khi bạn "rời khỏi", thì xóa đồng hồ
-            clearInterval(timerRef.current)
-        };
+        setTimeLeft(TIME);
 
-    }, []); // useEffect chỉ chạy 1 lần lúc "vào nhà"
+        timerRef.current = setTimeout(() => {
+            onTimeout?.(); // GỌI SAU KHI render xong
+        }, TIME * 1000);
+
+        // Code cần chạy khi component mount hoặc update
+        // Bạn lắp đồng hồ
+        intervalRef.current = setInterval(
+            () => {
+                //Đồng hồ tự chạy và trừ timeLeft mỗi giây
+                setTimeLeft((prev) => {
+                    if(prev <= 0) {
+                        clearInterval(timerRef.current); // dừng khi còn 0
+                        return 0;
+                    }
+                    return prev - 1;
+                })
+            }, 1000
+        );
+        return () => {
+                // Code cleanup (chạy khi component unmount hoặc trước khi chạy lại useEffect)
+                // Khi bạn "rời khỏi", thì xóa đồng hồ
+                clearInterval(intervalRef.current)
+                clearTimeout(timerRef.current)
+            };
+
+    }, [currentQuestionIndex]); // useEffect chỉ chạy 1 lần lúc "vào nhà"
 
 
     return(
